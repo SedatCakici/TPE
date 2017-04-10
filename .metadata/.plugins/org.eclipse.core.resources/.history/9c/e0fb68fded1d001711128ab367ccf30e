271d071b50939daf87b3ed8e1cab75a7d74ef734
sedat <sedat-cakici@hotmail.de>
2017-04-10 16:01:04 +0200

    ende

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/9c/e0fb68fded1d001711128ab367ccf30e
@@ -0,0 +1,11 @@
+package DeHsMannheimSS17TPEGrp01Übung2;
+
+import org.junit.Test;
+
+public class RentenpapierTest {
+
+	@Test
+	public void test() {
+	}
+
+}
---
+++ TPE/src/DeHsMannheimSS17TPEGrp01Übung2/Rentenfonds.java
@@ -0,0 +1,12 @@
+package DeHsMannheimSS17TPEGrp01Übung2;
+
+import java.util.ArrayList;
+
+public class Rentenfonds {
+
+	ArrayList<Rentenpapier> rentenpapierr = new ArrayList<Rentenpapier>();
+	
+	public void rentenAnsammlung(Rentenpapier e){
+		rentenpapierr.add(e);
+	}
+}
